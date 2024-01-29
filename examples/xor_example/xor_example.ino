220f6d003960990ead862acb02ca69df36b922aa
nthnn <nathanneisip@gmail.com>
2024-01-30 01:30:14 +0800

    Updated XOR example.

--- examples/xor_example/xor_example.ino
+++ examples/xor_example/xor_example.ino
@@ -6,18 +6,18 @@ void setup() {
 
     // Define training input and output data for XOR operation
     float trainingInput[4][2] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-    float trainingOutput[4][1] = {{0}, {1}, {1}, {0}};
+    float trainingOutput[4][1] = {{1}, {0}, {0}, {1}};
 
     // Create an instance of the Diwa neural network with 2 input neurons, 1 hidden layer with 3 neurons, and 1 output neuron
     Diwa network(2, 1, 3, 1);
 
     // Train the network for 3000 epochs using the XOR training data
-    for(int epoch = 0; epoch < 3000; epoch++) {
+    for(int epoch = 0; epoch < 4000; epoch++) {
         // Train the network for each set of input and target output values
-        network.train(4, trainingInput[0], trainingOutput[0]);
-        network.train(4, trainingInput[1], trainingOutput[1]);
-        network.train(4, trainingInput[2], trainingOutput[2]);
-        network.train(4, trainingInput[3], trainingOutput[3]);
+        network.train(5, trainingInput[0], trainingOutput[0]);
+        network.train(5, trainingInput[1], trainingOutput[1]);
+        network.train(5, trainingInput[2], trainingOutput[2]);
+        network.train(5, trainingInput[3], trainingOutput[3]);
     }
 
     // Perform inference on the trained network and print the results
