645bfe2657b7fe198335972cda8b915dac8b21e3
nthnn <nathanneisip@gmail.com>
2024-03-14 15:19:55 +0800

    Some fixes on XOR example.

--- examples/xor_example/xor_example.ino
+++ examples/xor_example/xor_example.ino
@@ -27,28 +27,30 @@ void setup() {
     Serial.begin(115200);
 
     // Define training input and output data for XOR operation
-    float trainingInput[4][2] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-    float trainingOutput[4][1] = {{1}, {0}, {0}, {1}};
+    double trainingInput[4][2] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+    double trainingOutput[4][1] = {{1}, {0}, {0}, {1}};
 
-    // Create an instance of the Diwa neural network with 2 input neurons, 1 hidden layer with 3 neurons, and 1 output neuron
-    Diwa network(2, 1, 3, 1);
+    // Create an instance of the Diwa neural network with 2 input neurons,
+    // 1 hidden layer with 3 neurons, and 1 output neuron
+    Diwa network;
+    network.initialize(2, 1, 3, 1);
 
     // Train the network for 3000 epochs using the XOR training data
-    for(int epoch = 0; epoch < 4000; epoch++) {
+    for(uint32_t epoch = 0; epoch < 10000; epoch++) {
         // Train the network for each set of input and target output values
-        network.train(5, trainingInput[0], trainingOutput[0]);
-        network.train(5, trainingInput[1], trainingOutput[1]);
-        network.train(5, trainingInput[2], trainingOutput[2]);
-        network.train(5, trainingInput[3], trainingOutput[3]);
+        network.train(6, trainingInput[0], trainingOutput[0]);
+        network.train(6, trainingInput[1], trainingOutput[1]);
+        network.train(6, trainingInput[2], trainingOutput[2]);
+        network.train(6, trainingInput[3], trainingOutput[3]);
     }
 
     // Perform inference on the trained network and print the results
-    for(int i = 0; i < 4; i++) {
+    for(uint8_t i = 0; i < 4; i++) {
         // Get the current input row
-        float* row = trainingInput[i];
+        double* row = trainingInput[i];
 
         // Perform inference using the trained network
-        float* inferred = network.inference(row);
+        double* inferred = network.inference(row);
 
         // Print the result for the current input
         char str[100];
@@ -57,4 +59,6 @@ void setup() {
     }
 }
 
-void loop() { }
+void loop() {
+  vTaskDelay(10);
+}
