d9238087f6c531eefe21914a306d18202102d33a
nthnn <nathanneisip@gmail.com>
2024-03-23 00:15:03 +0800

    Updated documentation pages for DiwaActivationFunc class.

--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['recommendedhiddenlayercount_0',['recommendedHiddenLayerCount',['../class_diwa.html#a8777a2d67f101047f7dfd6fb3d92bb01',1,'Diwa']]],
-  ['recommendedhiddenneuroncount_1',['recommendedHiddenNeuronCount',['../class_diwa.html#aee7f6dd835f93a7c88e890e5f329ccb6',1,'Diwa']]]
+  ['radialbasis_0',['radialBasis',['../class_diwa_activation_func.html#a177590ca0d50b8c3f6a6248ccae6d736',1,'DiwaActivationFunc']]],
+  ['recommendedhiddenlayercount_1',['recommendedHiddenLayerCount',['../class_diwa.html#a8777a2d67f101047f7dfd6fb3d92bb01',1,'Diwa']]],
+  ['recommendedhiddenneuroncount_2',['recommendedHiddenNeuronCount',['../class_diwa.html#aee7f6dd835f93a7c88e890e5f329ccb6',1,'Diwa']]]
 ];
