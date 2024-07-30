da6258fe4eccd426d8c10932960acff93a1ab299
Nathanne Isip <nathanneisip@gmail.com>
2024-07-31 04:44:55 +0800

    Updated documentation pages.

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -2,5 +2,13 @@ var searchData=
 [
   ['gaussian_0',['gaussian',['../classDiwaActivationFunc.html#a3f6893bca239e761c9429090503c1b0d',1,'DiwaActivationFunc']]],
   ['getactivationfunction_1',['getActivationFunction',['../classDiwa.html#a410c77ad562f6e49f9d79c4fde37ac3d',1,'Diwa']]],
-  ['getting_20started_2',['Getting Started',['../index.html#autotoc_md1',1,'']]]
+  ['gethiddenlayers_2',['getHiddenLayers',['../classDiwa.html#a48a14c006782b6703a6f9d64a7082fa0',1,'Diwa']]],
+  ['gethiddenneurons_3',['getHiddenNeurons',['../classDiwa.html#a025e99afe9422db6b257ecb8b9aa4439',1,'Diwa']]],
+  ['getinputneurons_4',['getInputNeurons',['../classDiwa.html#ae15ba3b0de7eda7498a5ac15569332bc',1,'Diwa']]],
+  ['getneuroncount_5',['getNeuronCount',['../classDiwa.html#a319cbe793401866d01113a0ec13d8a3a',1,'Diwa']]],
+  ['getoutputneurons_6',['getOutputNeurons',['../classDiwa.html#a7e882915fd77f4ceb4e55fdd113cf3c6',1,'Diwa']]],
+  ['getoutputs_7',['getOutputs',['../classDiwa.html#a322903a4c78a7397b681649adb52afbc',1,'Diwa']]],
+  ['getting_20started_8',['Getting Started',['../index.html#autotoc_md1',1,'']]],
+  ['getweightcount_9',['getWeightCount',['../classDiwa.html#acbeaff02e3cdcb29e4d237b360200028',1,'Diwa']]],
+  ['getweights_10',['getWeights',['../classDiwa.html#ab8816ebc8d033f2e6facc3f837fdc453',1,'Diwa']]]
 ];
--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,5 +1,13 @@
 var searchData=
 [
   ['gaussian_0',['gaussian',['../classDiwaActivationFunc.html#a3f6893bca239e761c9429090503c1b0d',1,'DiwaActivationFunc']]],
-  ['getactivationfunction_1',['getActivationFunction',['../classDiwa.html#a410c77ad562f6e49f9d79c4fde37ac3d',1,'Diwa']]]
+  ['getactivationfunction_1',['getActivationFunction',['../classDiwa.html#a410c77ad562f6e49f9d79c4fde37ac3d',1,'Diwa']]],
+  ['gethiddenlayers_2',['getHiddenLayers',['../classDiwa.html#a48a14c006782b6703a6f9d64a7082fa0',1,'Diwa']]],
+  ['gethiddenneurons_3',['getHiddenNeurons',['../classDiwa.html#a025e99afe9422db6b257ecb8b9aa4439',1,'Diwa']]],
+  ['getinputneurons_4',['getInputNeurons',['../classDiwa.html#ae15ba3b0de7eda7498a5ac15569332bc',1,'Diwa']]],
+  ['getneuroncount_5',['getNeuronCount',['../classDiwa.html#a319cbe793401866d01113a0ec13d8a3a',1,'Diwa']]],
+  ['getoutputneurons_6',['getOutputNeurons',['../classDiwa.html#a7e882915fd77f4ceb4e55fdd113cf3c6',1,'Diwa']]],
+  ['getoutputs_7',['getOutputs',['../classDiwa.html#a322903a4c78a7397b681649adb52afbc',1,'Diwa']]],
+  ['getweightcount_8',['getWeightCount',['../classDiwa.html#acbeaff02e3cdcb29e4d237b360200028',1,'Diwa']]],
+  ['getweights_9',['getWeights',['../classDiwa.html#ab8816ebc8d033f2e6facc3f837fdc453',1,'Diwa']]]
 ];
